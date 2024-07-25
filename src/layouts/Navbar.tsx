a972d90d4e760ceac4eac439cdf618041331b9de
Siddikpatel <patel.siddik369@gmail.com>
2024-07-25 08:26:33 +0530

    commit 4

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -5,15 +5,7 @@ import { useEffect, useRef, useState } from 'react';
 
 export const Navbar = () => {
 
-    // const showSidebar: any = (element: HTMLElement) => {
-    //     element.style.display = 'flex';
-    // }
-
-    // const hideSidebar: any = (element: HTMLElement) => {
-    //     element.style.display = 'none';
-    // }
-
-    const [isVisible, setIsVisible] = useState(false);
+    const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
     return (
 
@@ -35,31 +27,33 @@ export const Navbar = () => {
                         <li className={styles.hideOnMobile}>
                             Donate Me
                         </li>
-                        <li onClick={() => setIsVisible(true)}>
+                        <li onClick={() => setIsSidebarVisible(true)}>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
                         </li>
                     </ul>                    
                 </div>
-                <div className={`${styles.nav_mobile}`}>
-                    <ul className={`${styles.nav_elements}`}>
-                        <li onClick={() => setIsVisible(false)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-                        </li>
-                        <li>
-                            <NavLink to="/home">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about">
-                                About
-                            </NavLink>
-                        </li>
-                        <li>
-                            Donate Me
-                        </li>
-                    </ul>
-                </div>
+                {isSidebarVisible &&
+                    <div className={`${styles.nav_mobile}`}>
+                        <ul className={`${styles.nav_elements}`}>
+                            <li onClick={() => setIsSidebarVisible(false)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+                            </li>
+                            <li onClick={() => setIsSidebarVisible(false)}>
+                                <NavLink to="/home">
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li onClick={() => setIsSidebarVisible(false)}>
+                                <NavLink to="/about">
+                                    About
+                                </NavLink>
+                            </li>
+                            <li onClick={() => setIsSidebarVisible(false)}>
+                                Donate Me
+                            </li>
+                        </ul>
+                    </div>
+                }
             </div>
         </>
     );
